1114a2e183bd6c51796a8ad3e88ef458b4a4a532
Tracy Mu Sung <tracy.musung@moomumedia.com>
2015-09-26 16:25:06 +1000

    Don't use fmt anymore. Make 2 pages, a main and a results page.

--- guts.go
+++ guts.go
@@ -2,17 +2,25 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"html/template"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func resultsHandler(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("results.html")
+	t.Execute(w, r) // I put r here, because it needs 2 arguments, but I have no idea why this is ok
+}
+
+
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("main.html")
 	t.Execute(w, r) // I put r here, because it needs 2 arguments, but I have no idea why this is ok
 }
 
+
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/results", resultsHandler)
+	http.HandleFunc("/", homeHandler)
 	http.ListenAndServe("localhost:3000", nil)
 }
